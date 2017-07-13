9d63a653926c5beb89fddc0042c12d143d638233
Christian <wyqer@wyqer.net>
2017-07-13 09:17:33 +0200

    Tanoa RHS Version

---
+++ kp_support_rhs.Tanoa/cfgGroups.hpp
@@ -0,0 +1,313 @@
+class CfgGroups
+{
+	class West
+	{
+		name = $STR_A3_CfgGroups_West0;
+		side = 1;
+		class BLU_F
+		{
+			name = $STR_A3_CfgGroups_West_BLU_F0;
+			class Infantry
+			{
+				name = $STR_A3_CfgGroups_West_BLU_F_Infantry0;
+				class KP_Support_B4
+				{
+					name = $str_a3_cfggroups_west_blu_f_infantry_bus_reconteam0;
+					side = 1;
+					faction = BLU_F;
+					class Unit0		{side = 1; vehicle = rhsusf_usmc_recon_marpat_wd_teamleader_lite; rank = "SERGEANT";};
+					class Unit1		{side = 1; vehicle = rhsusf_usmc_recon_marpat_wd_rifleman_lite; rank = "CORPORAL";};
+					class Unit2		{side = 1; vehicle = rhsusf_usmc_recon_marpat_wd_rifleman_lite; rank = "PRIVATE";};
+					class Unit3		{side = 1; vehicle = rhsusf_usmc_recon_marpat_wd_marksman_lite; rank = "PRIVATE";};
+				};
+				class KP_Support_B8
+				{
+					name = $str_a3_cfggroups_west_blu_f_infantry_bus_infsquad0;
+					side = 1;
+					faction = BLU_F;
+					class Unit0		{side = 1; vehicle = rhsusf_usmc_marpat_wd_squadleader; rank = "SERGEANT";};
+					class Unit1		{side = 1; vehicle = rhsusf_usmc_marpat_wd_teamleader; rank = "SERGEANT";};
+					class Unit2		{side = 1; vehicle = rhsusf_usmc_marpat_wd_grenadier; rank = "CORPORAL";};
+					class Unit3		{side = 1; vehicle = rhsusf_usmc_marpat_wd_riflemanat; rank = "PRIVATE";};
+					class Unit4		{side = 1; vehicle = rhsusf_usmc_marpat_wd_rifleman; rank = "PRIVATE";};
+					class Unit5		{side = 1; vehicle = rhsusf_navy_marpat_wd_medic; rank = "CORPORAL";};
+					class Unit6 	{side = 1; vehicle = rhsusf_usmc_marpat_wd_autorifleman_m249; rank = "PRIVATE";};
+					class Unit7 	{side = 1; vehicle = rhsusf_usmc_marpat_wd_autorifleman_m249; rank = "PRIVATE";};
+				};
+				class KP_Support_B16
+				{
+					name = $str_a3_cfggroups_west_blu_f_infantry_bus_infsquad0;
+					side = 1;
+					faction = BLU_F;
+					class Unit0 	{side = 1; vehicle = rhsusf_usmc_marpat_wd_squadleader; rank = "SERGEANT";};
+					class Unit1 	{side = 1; vehicle = rhsusf_usmc_marpat_wd_teamleader; rank = "SERGEANT";};
+					class Unit2 	{side = 1; vehicle = rhsusf_usmc_marpat_wd_grenadier; rank = "CORPORAL";};
+					class Unit3 	{side = 1; vehicle = rhsusf_usmc_marpat_wd_autorifleman_m249; rank = "PRIVATE";};
+					class Unit4 	{side = 1; vehicle = rhsusf_usmc_marpat_wd_autorifleman_m249; rank = "PRIVATE";};
+					class Unit5 	{side = 1; vehicle = rhsusf_usmc_marpat_wd_riflemanat; rank = "CORPORAL";};
+					class Unit6 	{side = 1; vehicle = rhsusf_navy_marpat_wd_medic; rank = "PRIVATE";};
+					class Unit7 	{side = 1; vehicle = rhsusf_usmc_marpat_wd_autorifleman_m249; rank = "PRIVATE";};
+					class Unit8 	{side = 1; vehicle = rhsusf_usmc_marpat_wd_grenadier; rank = "CORPORAL";};
+					class Unit9 	{side = 1; vehicle = rhsusf_usmc_marpat_wd_autorifleman_m249; rank = "PRIVATE";};
+					class Unit10	{side = 1; vehicle = rhsusf_usmc_marpat_wd_rifleman; rank = "PRIVATE";};
+					class Unit11	{side = 1; vehicle = rhsusf_usmc_marpat_wd_riflemanat; rank = "CORPORAL";};
+					class Unit12	{side = 1; vehicle = rhsusf_navy_marpat_wd_medic; rank = "PRIVATE";};
+					class Unit13	{side = 1; vehicle = rhsusf_usmc_marpat_wd_rifleman; rank = "PRIVATE";};
+					class Unit14	{side = 1; vehicle = rhsusf_usmc_marpat_wd_smaw; rank = "CORPORAL";};
+					class Unit15	{side = 1; vehicle = rhsusf_usmc_marpat_wd_smaw; rank = "PRIVATE";};
+				};
+				class KP_Support_BAT
+				{
+					name = $STR_A3_CfgGroups_West_BLU_F_Infantry_BUS_InfTeam_AT0;
+					side = 1;
+					faction = BLU_F;
+					class Unit0		{side = 1; vehicle = rhsusf_usmc_marpat_wd_teamleader; rank = "SERGEANT";};
+					class Unit1		{side = 1; vehicle = rhsusf_usmc_marpat_wd_smaw; rank = "SERGEANT";};
+					class Unit2		{side = 1; vehicle = rhsusf_usmc_marpat_wd_smaw; rank = "PRIVATE";};
+					class Unit3		{side = 1; vehicle = rhsusf_usmc_marpat_wd_rifleman; rank = "PRIVATE";};
+				};
+				class KP_Support_BAA
+				{
+					name = $str_a3_cfggroups_west_blu_f_infantry_bus_infteam_aa0;
+					side = 1;
+					faction = BLU_F;
+					class Unit0		{side = 1; vehicle = rhsusf_usmc_marpat_wd_teamleader; rank = "SERGEANT";};
+					class Unit1		{side = 1; vehicle = rhsusf_usmc_marpat_wd_stinger; rank = "SERGEANT";};
+					class Unit2		{side = 1; vehicle = rhsusf_usmc_marpat_wd_stinger; rank = "PRIVATE";};
+					class Unit3		{side = 1; vehicle = rhsusf_usmc_marpat_wd_rifleman; rank = "PRIVATE";};
+				};
+			};
+			class Motorized
+			{
+				name = $STR_A3_CfgGroups_West_BLU_F_Motorized0;
+				class KP_Support_BMotInf_AT
+				{
+					name = $str_a3_cfggroups_west_blu_f_motorized_bus_motinf_at0;
+					side = 1;
+					faction = BLU_F;
+					class Unit0		{side = 1; vehicle = rhsusf_m998_w_s_4dr; rank = "SERGEANT";};
+					class Unit1		{side = 1; vehicle = rhsusf_usmc_marpat_wd_smaw; rank = "CORPORAL";};
+					class Unit2		{side = 1; vehicle = rhsusf_usmc_marpat_wd_smaw; rank = "CORPORAL";};
+					class Unit3		{side = 1; vehicle = rhsusf_usmc_marpat_wd_autorifleman_m249; rank = "PRIVATE";};
+				};
+				class KP_Support_BMotInf_GMG
+				{
+					name = $str_a3_cfggroups_west_blu_f_motorized_bus_motinf_team0;
+					side = 1;
+					faction = BLU_F;
+					class Unit0		{side = 1; vehicle = rhsusf_m1025_w_s_Mk19; rank = "SERGEANT";};
+					class Unit1		{side = 1; vehicle = rhsusf_usmc_marpat_wd_rifleman; rank = "CORPORAL";};
+					class Unit2		{side = 1; vehicle = rhsusf_usmc_marpat_wd_rifleman; rank = "PRIVATE";};
+				};
+				class KP_Support_BMotInf_HMG
+				{
+					name = $str_a3_cfggroups_west_blu_f_motorized_bus_motinf_team0;
+					side = 1;
+					faction = BLU_F;
+					class Unit0		{side = 1; vehicle = rhsusf_m1025_w_s_m2; rank = "SERGEANT";};
+					class Unit1		{side = 1; vehicle = rhsusf_usmc_marpat_wd_rifleman; rank = "CORPORAL";};
+					class Unit2		{side = 1; vehicle = rhsusf_usmc_marpat_wd_rifleman; rank = "PRIVATE";};
+				};
+			};
+			class Mechanized
+			{
+				name = $str_a3_cfggroups_west_blu_f_mechanized0;
+				class KP_Support_BMechL
+				{
+					name = $str_a3_cfggroups_west_blu_f_mechanized_bus_mechinfsquad0;
+					side = 1;
+					faction = BLU_F;
+					class Unit0		{side = 1; vehicle = rhsusf_M1232_M2_usarmy_wd; rank = "SERGEANT";};
+					class Unit1		{side = 1; vehicle = rhsusf_usmc_marpat_wd_squadleader; rank = "SERGEANT";};
+					class Unit2		{side = 1; vehicle = rhsusf_usmc_marpat_wd_teamleader; rank = "SERGEANT";};
+					class Unit3		{side = 1; vehicle = rhsusf_usmc_marpat_wd_grenadier; rank = "CORPORAL";};
+					class Unit4		{side = 1; vehicle = rhsusf_usmc_marpat_wd_autorifleman_m249; rank = "PRIVATE";};
+					class Unit5		{side = 1; vehicle = rhsusf_usmc_marpat_wd_autorifleman_m249; rank = "PRIVATE";};
+					class Unit6		{side = 1; vehicle = rhsusf_usmc_marpat_wd_riflemanat; rank = "CORPORAL";};
+					class Unit7		{side = 1; vehicle = rhsusf_navy_marpat_wd_medic; rank = "PRIVATE";};
+					class Unit8		{side = 1; vehicle = rhsusf_usmc_marpat_wd_rifleman; rank = "PRIVATE";};
+				};
+				class KP_Support_BMechH
+				{
+					name = $str_a3_cfggroups_west_blu_f_mechanized_bus_mechinfsquad0;
+					side = 1;
+					faction = BLU_F;
+					class Unit0		{side = 1; vehicle = RHS_M2A3_wd; rank = "SERGEANT";};
+					class Unit1		{side = 1; vehicle = rhsusf_usmc_marpat_wd_squadleader; rank = "SERGEANT";};
+					class Unit2		{side = 1; vehicle = rhsusf_usmc_marpat_wd_teamleader; rank = "SERGEANT";};
+					class Unit3		{side = 1; vehicle = rhsusf_usmc_marpat_wd_grenadier; rank = "CORPORAL";};
+					class Unit4		{side = 1; vehicle = rhsusf_usmc_marpat_wd_autorifleman_m249; rank = "PRIVATE";};
+					class Unit5		{side = 1; vehicle = rhsusf_usmc_marpat_wd_autorifleman_m249; rank = "PRIVATE";};
+					class Unit6		{side = 1; vehicle = rhsusf_usmc_marpat_wd_riflemanat; rank = "CORPORAL";};
+					class Unit7		{side = 1; vehicle = rhsusf_navy_marpat_wd_medic; rank = "PRIVATE";};
+					class Unit8		{side = 1; vehicle = rhsusf_usmc_marpat_wd_rifleman; rank = "PRIVATE";};
+				};
+			};
+			class Armored
+			{
+				class KP_Support_BArm
+				{
+					name = "Armored";
+					side = 1;
+					faction = BLU_F;
+					class Unit0		{side = 1; vehicle = rhsusf_m1a1fep_wd; rank = "SERGEANT";};
+				};
+			};
+		};
+	};
+	class East
+	{
+		name = $STR_A3_CfgGroups_East0;
+		side = 0;
+		class OPF_F
+		{
+			name = $STR_A3_CfgGroups_East_OPF_F0;
+			class Infantry
+			{
+				name = $STR_A3_CfgGroups_West_BLU_F_Infantry0;
+				class KP_Support_O4
+				{
+					name = $str_a3_cfggroups_west_blu_f_infantry_bus_reconteam0;
+					side = 0;
+					faction = OPF_F;
+					class Unit0		{side = 0; vehicle = rhs_vmf_recon_sergeant; rank = "SERGEANT";};
+					class Unit1		{side = 0; vehicle = rhs_vmf_recon_medic; rank = "CORPORAL";};
+					class Unit2		{side = 0; vehicle = rhs_vmf_recon_rifleman_scout_akm; rank = "PRIVATE";};
+					class Unit3		{side = 0; vehicle = rhs_vmf_recon_marksman; rank = "PRIVATE";};
+				};
+				class KP_Support_O8
+				{
+					name = $str_a3_cfggroups_west_blu_f_infantry_bus_infsquad0;
+					side = 0;
+					faction = OPF_F;
+					class Unit0		{side = 0; vehicle = rhs_vdv_sergeant; rank = "SERGEANT";};
+					class Unit1		{side = 0; vehicle = rhs_vdv_efreitor; rank = "SERGEANT";};
+					class Unit2		{side = 0; vehicle = rhs_vdv_grenadier; rank = "CORPORAL";};
+					class Unit3		{side = 0; vehicle = rhs_vdv_LAT; rank = "PRIVATE";};
+					class Unit4		{side = 0; vehicle = rhs_vdv_machinegunner; rank = "PRIVATE";};
+					class Unit5		{side = 0; vehicle = rhs_vdv_medic; rank = "PRIVATE";};
+					class Unit6		{side = 0; vehicle = rhs_vdv_machinegunner; rank = "PRIVATE";};
+					class Unit7		{side = 0; vehicle = rhs_vdv_rifleman; rank = "PRIVATE";};
+				};
+				class KP_Support_O16
+				{
+					name = $str_a3_cfggroups_west_blu_f_infantry_bus_infsquad0;
+					side = 0;
+					faction = OPF_F;
+					class Unit0 	{side = 0; vehicle = rhs_vdv_sergeant; rank = "SERGEANT";};
+					class Unit1 	{side = 0; vehicle = rhs_vdv_efreitor; rank = "SERGEANT";};
+					class Unit2 	{side = 0; vehicle = rhs_vdv_grenadier; rank = "CORPORAL";};
+					class Unit3 	{side = 0; vehicle = rhs_vdv_machinegunner; rank = "PRIVATE";};
+					class Unit4 	{side = 0; vehicle = rhs_vdv_machinegunner; rank = "PRIVATE";};
+					class Unit5 	{side = 0; vehicle = rhs_vdv_medic; rank = "PRIVATE";};
+					class Unit6 	{side = 0; vehicle = rhs_vdv_grenadier; rank = "CORPORAL";};
+					class Unit7 	{side = 0; vehicle = rhs_vdv_machinegunner; rank = "PRIVATE";};
+					class Unit8 	{side = 0; vehicle = rhs_vdv_machinegunner; rank = "PRIVATE";};
+					class Unit9 	{side = 0; vehicle = rhs_vdv_medic; rank = "PRIVATE";};
+					class Unit10	{side = 0; vehicle = rhs_vdv_rifleman; rank = "PRIVATE";};
+					class Unit11	{side = 0; vehicle = rhs_vdv_LAT; rank = "CORPORAL";};
+					class Unit12	{side = 0; vehicle = rhs_vdv_LAT; rank = "PRIVATE";};
+					class Unit13	{side = 0; vehicle = rhs_vdv_rifleman; rank = "PRIVATE";};
+					class Unit14	{side = 0; vehicle = rhs_vdv_LAT; rank = "PRIVATE";};
+					class Unit15	{side = 0; vehicle = rhs_vdv_LAT; rank = "PRIVATE";};
+				};
+				class KP_Support_OAT
+				{
+					name = $STR_A3_CfgGroups_West_BLU_F_Infantry_BUS_InfTeam_AT0;
+					side = 0;
+					faction = OPF_F;
+					class Unit0		{side = 0; vehicle = rhs_vdv_efreitor; rank = "SERGEANT";};
+					class Unit1		{side = 0; vehicle = rhs_vdv_at; rank = "SERGEANT";};
+					class Unit2		{side = 0; vehicle = rhs_vdv_at; rank = "PRIVATE";};
+					class Unit3		{side = 0; vehicle = rhs_vdv_rifleman; rank = "PRIVATE";};
+				};
+				class KP_Support_OAA
+				{
+					name = $str_a3_cfggroups_west_blu_f_infantry_bus_infteam_aa0;
+					side = 0;
+					faction = OPF_F;
+					class Unit0		{side = 0; vehicle = rhs_vdv_efreitor; rank = "SERGEANT";};
+					class Unit1		{side = 0; vehicle = rhs_vdv_aa; rank = "SERGEANT";};
+					class Unit2		{side = 0; vehicle = rhs_vdv_aa; rank = "PRIVATE";};
+					class Unit3		{side = 0; vehicle = rhs_vdv_rifleman; rank = "PRIVATE";};
+				};
+			};
+			class Motorized
+			{
+				name = $STR_A3_cfggroups_East_OPF_F_Motorized_MTP0;
+				class KP_Support_OMotInf_AT
+				{
+					name = $str_a3_cfggroups_east_opf_f_motorized_mtp_oia_motinf_at0;
+					side = 0;
+					faction = OPF_F;
+					class Unit0		{side = 0; vehicle = rhs_tigr_vdv; rank = "SERGEANT";};
+					class Unit1		{side = 0; vehicle = rhs_vdv_at; rank = "CORPORAL";};
+					class Unit2		{side = 0; vehicle = rhs_vdv_at; rank = "PRIVATE";};
+					class Unit3		{side = 0; vehicle = rhs_vdv_machinegunner; rank = "PRIVATE";};
+				};
+				class KP_Support_OMotInf_HMG
+				{
+					name = $str_a3_cfggroups_east_opf_f_motorized_mtp_oia_motinfteam0;
+					side = 0;
+					faction = OPF_F;
+					class Unit0		{side = 0; vehicle = rhs_tigr_m_vdv; rank = "SERGEANT";};
+					class Unit1		{side = 0; vehicle = rhs_vdv_rifleman; rank = "CORPORAL";};
+					class Unit2		{side = 0; vehicle = rhs_vdv_rifleman; rank = "PRIVATE";};
+				};
+				class KP_Support_OMotInf_GMG
+				{
+					name = $str_a3_cfggroups_east_opf_f_motorized_mtp_oia_motinfteam0;
+					side = 0;
+					faction = OPF_F;
+					class Unit0		{side = 0; vehicle = rhs_tigr_sts_vdv; rank = "SERGEANT";};
+					class Unit1		{side = 0; vehicle = rhs_vdv_rifleman; rank = "CORPORAL";};
+					class Unit2		{side = 0; vehicle = rhs_vdv_rifleman; rank = "PRIVATE";};
+				};
+			};
+			class Mechanized
+			{
+				name = $str_a3_cfggroups_east_opf_f_mechanized0;
+				class KP_Support_OMechL
+				{
+					name = $STR_A3_cfggroups_West_BLU_F_Mechanized_BUS_MechInfSquad0;
+					side = 0;
+					faction = OPF_F;
+					class Unit0		{side = 0; vehicle = rhs_btr80_vdv; rank = "SERGEANT";};
+					class Unit1		{side = 0; vehicle = rhs_vdv_sergeant; rank = "SERGEANT";};
+					class Unit2		{side = 0; vehicle = rhs_vdv_efreitor; rank = "SERGEANT";};
+					class Unit3		{side = 0; vehicle = rhs_vdv_grenadier; rank = "CORPORAL";};
+					class Unit4		{side = 0; vehicle = rhs_vdv_machinegunner; rank = "PRIVATE";};
+					class Unit5		{side = 0; vehicle = rhs_vdv_machinegunner; rank = "PRIVATE";};
+					class Unit6		{side = 0; vehicle = rhs_vdv_LAT; rank = "CORPORAL";};
+					class Unit7		{side = 0; vehicle = rhs_vdv_medic; rank = "PRIVATE";};
+					class Unit8		{side = 0; vehicle = rhs_vdv_rifleman; rank = "PRIVATE";};
+				};
+				class KP_Support_OMechH
+				{
+					name = $STR_A3_cfggroups_West_BLU_F_Mechanized_BUS_MechInfSquad0;
+					side = 0;
+					faction = OPF_F;
+					class Unit0		{side = 0; vehicle = rhs_bmp2d_vdv; rank = "SERGEANT";};
+					class Unit1		{side = 0; vehicle = rhs_vdv_sergeant; rank = "SERGEANT";};
+					class Unit2		{side = 0; vehicle = rhs_vdv_efreitor; rank = "SERGEANT";};
+					class Unit3		{side = 0; vehicle = rhs_vdv_grenadier; rank = "CORPORAL";};
+					class Unit4		{side = 0; vehicle = rhs_vdv_machinegunner; rank = "PRIVATE";};
+					class Unit5		{side = 0; vehicle = rhs_vdv_machinegunner; rank = "PRIVATE";};
+					class Unit6		{side = 0; vehicle = rhs_vdv_LAT; rank = "CORPORAL";};
+					class Unit7		{side = 0; vehicle = rhs_vdv_medic; rank = "PRIVATE";};
+					class Unit8		{side = 0; vehicle = rhs_vdv_rifleman; rank = "PRIVATE";};
+				};
+			};
+			class Armored
+			{
+				class KP_Support_OArm
+				{
+					name = "Armored";
+					side = 1;
+					faction = OPF_F;
+					class Unit0		{side = 0; vehicle = rhs_t90a_tv; rank = "SERGEANT";};
+				};
+			};
+		};
+	};
+};
